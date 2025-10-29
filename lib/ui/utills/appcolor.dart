a520e3aefd4a4dbd7e9b7e5715434f80b077efb0
mohammedkhattab <khattabk205@gmail.com>
2025-10-29 17:57:17 +0300

    first commit

---
+++ lib/ui/utills/appcolor.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Appcolor {
+  static const Color white = Color(0xfff2feff);
+  static const Color purple = Color(0xff5669ff);
+  static const Color black = Color(0xff1c1c1c);
+  static const Color gray = Color(0xff7b7b7b);
+  static const Color cl = Color(0xfff2feff);
+  static const Color white = Color(0xfff2feff);
+  static const Color white = Color(0xfff2feff);
+  static const Color white = Color(0xfff2feff);
+}
